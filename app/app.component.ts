5bc0aeab59a813e32c25581d3375836ac398ec57
fang du <fxdu14@smumn.edu>
2016-06-26 10:45:04 -0500

    eEmployee project based on Angularjs2

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'eemployee-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls:['app/app.component.css']
+})
+export class AppComponent { }
